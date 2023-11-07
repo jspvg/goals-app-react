ca25844d93241a26b496035f0c8def630135b994
jspvg <josipovic1501@gmail.com>
2023-11-07 16:26:01 +0100

    added deletion functionality

--- src/App.tsx
+++ src/App.tsx
@@ -18,13 +18,17 @@ export default function App() {
     });
   };
 
+  const handleDeleteGoal = (id: number) => {
+    setGoals((prevGoals) => prevGoals.filter((goal) => goal.id !== id));
+  };
+
   return (
     <main>
       <Header image={{ src: goalsImage, alt: "A list of goals" }}>
         <h1>Your Course Goals</h1>
       </Header>
       <button onClick={handleAddGoal}>Add Goal</button>
-      <GoalList goals={goals}/>
+      <GoalList goals={goals} onDeleteGoal={handleDeleteGoal} />
     </main>
   );
 }
